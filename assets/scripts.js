d5c3474fe55031d55be3404b44e88f0b16e663d2
direktor2007 <>
2023-06-30 00:56:40 +0700

    added control HW3

---
+++ assets/scripts.js
@@ -0,0 +1,22 @@
+const number = +prompt('Сколько Вам лет?', '');
+
+switch  (number) {
+  case number == 18:
+    alert('Вы совершеннолетний, теперь всё можно!');
+    break;
+
+  case number == 10:
+    alert('Вы маленький, у ну-ка спать! ');
+    break;
+
+  case number == 30:
+    alert('Ложитесь спать, завтра на работу');
+    break;
+  default:
+        alert('Нет таких значений')
+}
+   
+    var name = prompt("Ваше имя?", "");
+        alert("Ваше имя - " + name + "!");  
+    
+    
